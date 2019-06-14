6fcb7835db4ada0daff278c245866c06758cfe30
Xiaoyu Luo <xluoforhire@gmail.com>
2019-06-14 15:52:19 -0400

    hello world

---
+++ spring-boot-01-hello/src/main/java/com/mengxuegu/springboot/HelloMainApplication.java
@@ -0,0 +1,13 @@
+package com.mengxuegu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HelloMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMainApplication.class, args);
+    }
+}
