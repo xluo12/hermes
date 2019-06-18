ea3810e78d69f9f114959dd5db680fccec574be0
Xiaoyu Luo <xluoforhire@gmail.com>
2019-06-18 18:09:54 -0400

    supplier list

--- spring-boot-05-bill/src/main/java/com/xluo12/springboot/controller/ProviderController.java
+++ spring-boot-05-bill/src/main/java/com/xluo12/springboot/controller/ProviderController.java
@@ -1,18 +1,29 @@
 package com.xluo12.springboot.controller;
 
 
+import com.xluo12.springboot.dao.ProviderDao;
+import com.xluo12.springboot.entities.Provider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Collection;
+import java.util.Map;
+
 @Controller
 public class ProviderController {
 
+    @Autowired
+    ProviderDao providerDao;
+
     @GetMapping("/providers")
-    public String list() {
+    public String list(Map<String, Object> map) {
+
+        Collection<Provider> providers = providerDao.getAll();
+        map.put("providers", providers);
         return "provider/list";
-//        return "main/public";
-    }
+     }
 
 }
